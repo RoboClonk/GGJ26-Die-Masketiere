4dcb2c25a773849173b5684badfc6c7f0a83ac89
bitowl <git@bitowl.de>
2026-01-31 15:48:50 +0100

    Add mask interactable

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -2,3 +2,14 @@ extends Node
 
 var player_health: float = 10
 signal attack_signal(duration: float)
+
+enum Levels {
+	Stoneage,
+	Antique
+}
+
+# Mask count in each level.
+var mask_count: Array[int] = [0, 0]
+
+signal mask_collected(mask_count: int)
+	
