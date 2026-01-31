c083f62258cb62a23ccbd4024cb5d9c91eb03a39
bitowl <git@bitowl.de>
2026-01-31 18:01:51 +0100

    Test with 2d lighting

--- scenes/interactables/pedestal.gd
+++ scenes/interactables/pedestal.gd
@@ -3,11 +3,11 @@ extends Interactable
 
 @onready var pedestal_item: Sprite2D = $PedestalItem
 
-@export_range(33, 37) var pedestal_item_frame: int = 33:
+@export_range(0, 4) var pedestal_item_frame: int = 0:
 	set(value):
 		pedestal_item_frame = value
 		if pedestal_item:
-			pedestal_item.frame = value
+			pedestal_item.frame = value + 37
 
 
 func interact() -> void:
