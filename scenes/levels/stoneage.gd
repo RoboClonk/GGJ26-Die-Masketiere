138dfb1cd1ca8babc7ac732592bc2cfffc7f2d95
Marakasch <mark.seitz99@gmail.com>
2026-02-01 11:37:51 +0100

    Stoneage level finished

---
+++ scenes/levels/stoneage.gd
@@ -0,0 +1,22 @@
+extends Level
+@export var closed_door_one: Door
+@export var closed_door_two: Door
+@export var fake_pedestral: StaticBody2D
+@export var fake_pedestral_2: StaticBody2D
+@export var fake_pedestral_3: StaticBody2D
+@export var fake_pedestral_4: StaticBody2D
+
+
+
+
+func _on_button_pressed_open_door() -> void:
+	closed_door_one.state = Door.State.OPEN
+
+
+
+func _on_button_pressed_open_second_door() -> void:
+	closed_door_two.state = Door.State.OPEN
+	fake_pedestral.queue_free()
+	fake_pedestral_2.queue_free()
+	fake_pedestral_3.queue_free()
+	fake_pedestral_4.queue_free()
