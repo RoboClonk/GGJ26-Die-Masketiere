8e67e7b0790f40d8e24d278863175c536ac5da35
Robin Hohnsbeen <robin@hohnsbeen.de>
2026-02-02 18:56:40 +0100

    Fixed overlap check position

--- overlap_check.gd
+++ overlap_check.gd
@@ -3,25 +3,24 @@ class_name OverlapCheck
 
 var radius : float = 50.0
 var maximum_intersections : int = 6
+var remove = false
 
 signal on_colliders_collected(intersections : Array[Dictionary])
 
 static func create_physics_overlap(in_context : Node, in_position : Vector2, in_radius : float, in_maximum_intersections : int = 6) -> OverlapCheck:
-	#var new_overlap_check = OverlapCheck.new(in_radius, in_maximum_intersections)
 	var new_overlap_check = OverlapCheck.new()
 	new_overlap_check.radius = in_radius
 	new_overlap_check.maximum_intersections = in_maximum_intersections
+	in_context.get_tree().root.add_child(new_overlap_check)
 	new_overlap_check.global_position = in_position
-	in_context.add_child(new_overlap_check)
 	return new_overlap_check
-	
 
-#func _setup(in_radius : float, in_maximum_intersections : int = 6) -> void:
-#	radius = in_radius
-#	maximum_intersections = in_maximum_intersections
-	
 
 func _physics_process(delta: float) -> void:
+	if remove:
+		queue_free()
+		return
+	
 	var circle_rid = PhysicsServer2D.circle_shape_create()
 	PhysicsServer2D.shape_set_data(circle_rid, radius)
 	var params = PhysicsShapeQueryParameters2D.new()
@@ -33,6 +32,6 @@ func _physics_process(delta: float) -> void:
 	var intersections : Array[Dictionary] = space_state.intersect_shape(params, maximum_intersections)
 	on_colliders_collected.emit(intersections)
 	PhysicsServer2D.free_rid(circle_rid)
-		
-	queue_free()
+	
+	remove = true
 	
